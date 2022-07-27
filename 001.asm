f6ff2bf4dc4585cd68a3f41a56a257915b459921
Charith47 <charithdissanayake47@gmail.com>
2022-07-27 11:44:31 +0530

    first assembly program

---
+++ 001.asm
@@ -0,0 +1,11 @@
+# assembly learning 001: A syscall
+.global _start
+.intel_syntax
+.section .text
+
+_start:
+    mov %eax, 1         # exit
+    mov %ebx, 65        # error code
+    int 0x80            # execute syscall
+
+.section .data
