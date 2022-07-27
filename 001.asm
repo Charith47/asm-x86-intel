3e5286d62f039292827cac5429fb3c6c6489ca3f
Charith47 <charithdissanayake47@gmail.com>
2022-07-27 11:49:03 +0530

    replace # with ;

--- 001.asm
+++ 001.asm
@@ -1,11 +1,11 @@
-# assembly learning 001: A syscall
+; assembly learning 001: A syscall
 .global _start
 .intel_syntax
 .section .text
 
 _start:
-    mov %eax, 1         # exit
-    mov %ebx, 65        # error code
-    int 0x80            # execute syscall
+    mov %eax, 1         ; exit
+    mov %ebx, 65        ; error code
+    int 0x80            ; execute syscall
 
 .section .data
--- 002.asm
+++ 002.asm
@@ -1,20 +1,20 @@
-# assembly learning 002: Hello World
+; assembly learning 002: Hello World
 .global _start
 .intel_syntax
 .section .text
 
 _start:
-    # write hello world to stdout
-    mov %eax, 4             # 4 th syscall is write
-    mov %ebx, 1             # 1 is STDOUT
-    lea %ecx, [message]     # mem addr of char buffer (lea in intel syntax)
-    mov %edx, 13            # len
+    ; write hello world to stdout
+    mov %eax, 4             ; 4 th syscall is write
+    mov %ebx, 1             ; 1 is STDOUT
+    lea %ecx, [message]     ; mem addr of char buffer (lea in intel syntax)
+    mov %edx, 13            ; len
     int 0x80
 
-    # exit the program with error code 0
-    mov %eax, 1             # 1 st syscall is exit
-    mov %ebx, 0             # error code 0
-    int 0x80                # execute syscall
+    ; exit the program with error code 0
+    mov %eax, 1             ; 1 st syscall is exit
+    mov %ebx, 0             ; error code 0
+    int 0x80                ; execute syscall
 
 .section .data
     message: .ascii "Hello World!\n"
